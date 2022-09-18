b52b22f88430ef48ee5523fd34458caf83779dcc
alexej-anosov <63290732+alexej-anosov@users.noreply.github.com>
2022-09-18 15:28:24 +0300

    Update hw2.1.py

--- hw2.1.py
+++ hw2.1.py
@@ -1,33 +1,33 @@
 class CountVectorizer(object):
     def __init__(self):
-        self._tokens = []
-        self._num_of_tokens = len(self._tokens)
+        self.__tokens = []
+        self.__num_of_tokens = len(self.__tokens)
 
     """we get a list of unique words of the text that we get in fit_transform method 
     and length of this list
     as properties of class instance"""
-    def _tokenize(self, corpus):
+    def __tokenize(self, corpus):
         for string in corpus:
-            self._tokens.extend(map(lambda x: x.lower(), string.split()))
-        self._tokens = list(set(self._tokens))
-        self._num_of_tokens = len(self._tokens)
+            self.__tokens.extend(map(lambda x: x.lower(), string.split()))
+        self.__tokens = list(set(self.__tokens))
+        self.__num_of_tokens = len(self.__tokens)
 
     """print unique words list"""
     def get_feature_names(self):
-        return self._tokens
+        return self.__tokens
 
     """we get the text(List of string), 
     tokenize it,
     count how many times each word occurs in each string
     and return document-term matrix as list of lists"""
     def fit_transform(self, corpus):
-        self._tokenize(corpus)
+        self.__tokenize(corpus)
         output = []
         for string in corpus:
-            string_output = [0]*self._num_of_tokens
+            string_output = [0]*self.__num_of_tokens
             for index, word in enumerate(string.split()):
                 i = 0
-                while word.lower() != self._tokens[i]:
+                while word.lower() != self.__tokens[i]:
                     i += 1
                 string_output[i] += 1
             output.append(string_output)
