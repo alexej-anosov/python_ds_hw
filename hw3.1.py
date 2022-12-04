28126da697594fb023d86ac8ef3b12c93ad83e8f
alexej-anosov <63290732+alexej-anosov@users.noreply.github.com>
2022-12-04 18:54:19 +0300

    Update hw3.1.py

--- hw3.1.py
+++ hw3.1.py
@@ -1,56 +1,76 @@
 import json
+import keyword
 
 
-"""создать атрибут _repr в классе-потомке и в него ввести то, что необходимо вывести по функции repr"""
-class ColorizeMixin(object):
-    repr_color_code = 35
+class ColorizeMixin:
+    """создать атрибут _repr в классе-потомке и в него ввести то, что необходимо вывести по функции repr"""
+    repr_color_code = 0
 
     def __repr__(self):
-        return "\033[1;0;%im%s\033[0m" % (self.repr_color_code, self._repr)
+        return "\033[1;0;%im%s\033[0m" % (self.repr_color_code, self._to_repr())
 
 
-"""fff"""
-class AttributesDict(object):
+class AttributesDict:
+    """если значение атрибута класса Advert это словарь, то он в аттрибуте хранится объект данного класса"""
     def __init__(self, original_dictionary):
-        self.dictionary = {}
-        for key, obj in original_dictionary.items():
-            if type(obj) is not dict:
-                self.dictionary[key] = obj
+        self.__dictionary = original_dictionary
+        for key, value in original_dictionary.items():
+            if keyword.iskeyword(key):
+                key = f'{key}_'
+            if type(value) is dict:
+                setattr(self, key, AttributesDict(value))
             else:
-                self.dictionary[key] = AttributesDict(obj)
-        self.__dict__.update(self.dictionary)
-        self.__original_object = original_dictionary
+                setattr(self, key, value)
 
     def __repr__(self):
-        return str(self.__original_object)
+        return str(self.__dictionary)
 
 
-class Advert(ColorizeMixin, object):
+class Advert(ColorizeMixin):
+
+    repr_color_code = 34
+    _price = 0
+
     def __init__(self, dictionary):
-        a = AttributesDict(dictionary)
-        self.__dict__.update(a.dictionary)
-        if 'price' in self.__dict__:
-            if self.price < 0:
-                raise ValueError('Price must be >= 0')
-        else:
-            self.price = 0
+        for key, value in dictionary.items():
+            if keyword.iskeyword(key):
+                key = f'{key}_'
+            if type(value) is dict:
+                setattr(self, key, AttributesDict(value))
+            else:
+                if key == 'price':
+                    self._price = value
+                else:
+                    setattr(self, key, value)
 
-        self._repr = f'{self.title} | {self.price} ₽'
+    @property
+    def price(self):
+        return self._price
 
-    repr_color_code = 35
+    @price.setter
+    def price(self, price_value):
+        if price_value < 0:
+            raise ValueError('Price must be >= 0')
+        self._price = price_value
+
+    def _to_repr(self):
+        return f'{self.title} | {self.price} ₽'
 
 
-lesson_str = """{
-"title": "python", "price": 0,
-"location": {
-"address": "город Москва, Лесная, 7",
-"transport": {"metro_stations": ["Белорусская"], "buses": [1 ,2,3]}}
-}"""
+if __name__ == '__main__':
 
-lesson = json.loads(lesson_str)
-lesson_ad = Advert(lesson)
-print(lesson_ad.title)
-print(lesson_ad.price)
-print(lesson_ad)
-print(lesson_ad.location.transport)
-print(lesson_ad.location.transport.metro_stations)
+    lesson_str = """{
+    "title": "python", "price": 1,
+    "location": {
+    "address": "город Москва, Лесная, 7",
+    "transport": {"metro_stations": ["Белорусская"], "buses": [1,2,3]}}
+    }"""
+
+    lesson = json.loads(lesson_str)
+    lesson_ad = Advert(lesson)
+    print(lesson_ad.title)
+    print(lesson_ad.price)
+    lesson_ad.price = 2
+    print(lesson_ad)
+    print(lesson_ad.location.transport)
+    print(lesson_ad.location.transport.metro_stations)
